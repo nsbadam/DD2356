78d71da840caf6685e70bfdbf6fd1c22bf2ca6c8
nsbadam <131359880+nsbadam@users.noreply.github.com>
2023-04-21 16:15:37 +0200

    Create omp_critical_sum.c

---
+++ omp_critical_sum.c
@@ -0,0 +1,41 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "omp.h"
+#define SIZE 100000000
+double input[SIZE];
+
+void generate_random(double *input, size_t size)
+{
+  for (size_t i = 0; i < size; i++) {
+    #pragma omp critical
+    input[i] = rand() / (double)(RAND_MAX);
+  }
+}
+
+
+double serial_sum(double *x, size_t size)
+{
+  omp_set_num_threads(8);
+  double sum_val = 0.0;
+
+  for (size_t i = 0; i < size; i++) {
+    #pragma omp critical
+    sum_val += x[i];
+  }
+
+  return sum_val;
+}
+
+int main(int argc, char *argv[])
+{
+  double sum;
+
+  double start_time = omp_get_wtime();
+
+  generate_random(input, SIZE);
+  sum = serial_sum(input, SIZE);
+
+  double run_time = omp_get_wtime() - start_time;
+  printf("%f\n", run_time);
+  return 0;
+}
